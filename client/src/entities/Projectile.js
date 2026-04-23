a589e6367efb0c717473202deaba05a35ed0b637
Andreas de Cros Dich <andr9633@undervisning.kk.dk>
2026-04-23 21:49:19 +0200

    tune: POWER_FACTOR 0.09 -> 0.5

--- client/src/entities/Projectile.js
+++ client/src/entities/Projectile.js
@@ -6,7 +6,7 @@ import { GAME_CONFIG } from '@shared/constants.js';
  * powerFactor tuned so 100% power at 45° crosses most of the 800px world.
  */
 
-const POWER_FACTOR = 0.09; // velocity = power * POWER_FACTOR
+const POWER_FACTOR = 0.5; // velocity = power * POWER_FACTOR
 
 class Projectile {
     /**
